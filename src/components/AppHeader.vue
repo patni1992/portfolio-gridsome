1206bba1e2714ebedd99b4b9fda665eba47fe424
Patrik <patriknilsson1992@gmail.com>
2019-12-27 00:57:23 +0100

    add header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <b-navbar>
+        <template slot="start">
+            <b-navbar-item href="#">
+                Home
+            </b-navbar-item>
+            <b-navbar-item href="#">
+                Projects
+            </b-navbar-item>
+             <b-navbar-item href="#">
+               Contact
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+</template>
+<script>
+export default {
+
+};
+</script>
