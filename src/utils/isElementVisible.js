65a28129f7b903b101aebea5c2a5d8424c33ec02
Patrik <patriknilsson1992@gmail.com>
2019-12-27 19:55:46 +0100

    add isElementVisible utils

---
+++ src/utils/isElementVisible.js
@@ -0,0 +1,34 @@
+function isElementVisible(selector) {
+  const el = document.querySelector(selector);
+
+ if(!el) {
+     return false;
+ }
+
+  const rect = el.getBoundingClientRect();
+  const vWidth = window.innerWidth || doc.documentElement.clientWidth;
+  const vHeight = window.innerHeight || doc.documentElement.clientHeight;
+  
+  const efp = function(x, y) {
+    return document.elementFromPoint(x, y);
+  };
+
+  // Return false if it's not in the viewport
+  if (
+    rect.right < 0 ||
+    rect.bottom < 0 ||
+    rect.left > vWidth ||
+    rect.top > vHeight
+  )
+    return false;
+
+  // Return true if any of its four corners are visible
+  return (
+    el.contains(efp(rect.left, rect.top)) ||
+    el.contains(efp(rect.right, rect.top)) ||
+    el.contains(efp(rect.right, rect.bottom)) ||
+    el.contains(efp(rect.left, rect.bottom))
+  );
+}
+
+export default isElementVisible;
