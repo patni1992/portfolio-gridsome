8c9f71b0cbbd9bf33950ca5ed9b4f8bb65482317
Patrik <patriknilsson1992@gmail.com>
2019-12-27 19:56:15 +0100

    add footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,19 @@
+<template>
+     <footer class="app-footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>©{{ new Date().getFullYear() }} Patrik Nilsson</strong>
+        </p>
+      </div>
+    </footer>
+</template>
+<script>
+export default {
+
+};
+</script>
+<style lang="scss" scoped>
+    .app-footer {
+        padding-bottom: 2rem;
+    }
+</style>
